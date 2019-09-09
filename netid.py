093c42aa3da02cf7bbc3ceb5c3574c6b1cc5526f
GenerousAdmiRal <smitsshah8@gmail.com>
2019-09-09 15:43:14 +0530

    added mac address

--- netid.py
+++ netid.py
@@ -4,9 +4,10 @@ import uuid
 hostname = socket.gethostname()
 IPaddress = socket.gethostbyname(hostname)
 hexIP = socket.inet_aton(IPaddress).hex()
-#macaddress= ':'.join(['{ :02x}'.format(uuid.getnode() >> elements) & 0xff) for elements in range(0,2*6,2)][::-1])
+macaddress=  (':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
+for ele in range(0,8*6,8)][::-1]))
 
 print("The Computer name is: " +hostname)
 print("The Computer IP is: " +IPaddress)
 print("The Computer hexIP is: " +hexIP)
-#print("The Computer MAC is: " +macaddress)
+print("The Computer MAC is: " +macaddress)
